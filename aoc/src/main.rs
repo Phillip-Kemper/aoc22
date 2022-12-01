4731fee3fde8999cdf7e3c0331cba5cfab5d14b7
phillip-kemper <phillip-kemper@gmx.net>
2022-12-02 00:21:18 +0100

    task 1

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -1,3 +1,27 @@
+//use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "src/day1.txt";
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let chunks: Vec<&str> = contents.split("\n\n").collect();
+
+    let mut max = 0;
+
+    for chunk in chunks.into_iter() {
+        let energies: Vec<&str> = chunk.split("\n").collect();
+        println!("{:?}", energies);
+        let mut sum = 0;
+        for number in energies.into_iter() {
+            let num: i32 = number.parse().unwrap();
+            sum = sum + num;
+        }
+        if sum > max {
+            max = sum;
+        }
+    }
+
+    println!("Max: {:?}", max);
 }
