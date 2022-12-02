66a6c563d3e4f7633480e5c4c5d1c06d0321a526
phillip-kemper <phillip-kemper@gmx.net>
2022-12-02 10:10:27 +0100

    set up day2 and read file accordingly

--- aoc/src/bin/day2/main.rs
+++ aoc/src/bin/day2/main.rs
@@ -2,35 +2,43 @@
 use std::fs;
 
 fn main() {
-    let file_path = "src/day1.txt";
+    let file_path = "day2/day2.txt";
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let chunks: Vec<&str> = contents.split("\n\n").collect();
+    let rounds: Vec<&str> = contents.split("\n").collect();
 
-    let mut max = 0;
-    let mut max_array: [i32; 3] = [0; 3];
+    let mut total_sum = 0;
 
-    for chunk in chunks.into_iter() {
-        let energies: Vec<&str> = chunk.split("\n").collect();
-        println!("{:?}", energies);
-        let mut sum = 0;
-        for number in energies.into_iter() {
-            let num: i32 = number.parse().unwrap();
-            sum = sum + num;
-        }
-        if sum > max {
-            max = sum;
-        }
+    for round in rounds.iter() {
+        let choices: Vec<&str> = round.split(" ").collect();
 
-        let current_min = max_array.into_iter().min().unwrap();
-        if sum > current_min {
-            let index = max_array.iter().position(|&r| r == current_min).unwrap();
-            max_array[index] = sum;
-        }
+        let opponent_choice = choices.first().unwrap();
+        let proposed_choice = choices.last().unwrap();
+        println!("Choice 1: {:?}", opponent_choice);
+        println!("Choice 2: {:?}", proposed_choice);
     }
 
-    let total_sum: i32 = max_array.iter().sum();
-    println!("Max: {:?}", max_array);
-    println!("Max 3 Sum: {:?}", total_sum)
+    // for chunk in chunks.into_iter() {
+    //     let energies: Vec<&str> = chunk.split("\n").collect();
+    //     println!("{:?}", energies);
+    //     let mut sum = 0;
+    //     for number in energies.into_iter() {
+    //         let num: i32 = number.parse().unwrap();
+    //         sum = sum + num;
+    //     }
+    //     if sum > max {
+    //         max = sum;
+    //     }
+
+    //     let current_min = max_array.into_iter().min().unwrap();
+    //     if sum > current_min {
+    //         let index = max_array.iter().position(|&r| r == current_min).unwrap();
+    //         max_array[index] = sum;
+    //     }
+    // }
+
+    // let total_sum: i32 = max_array.iter().sum();
+    // println!("Max: {:?}", max_array);
+    // println!("Max 3 Sum: {:?}", total_sum)
 }
