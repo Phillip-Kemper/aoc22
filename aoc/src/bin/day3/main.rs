80d3bce5cbcac4a6676d6ff28bf4e192634d9307
phillip-kemper <phillip-kemper@gmx.net>
2022-12-03 10:39:40 +0100

    add substring dependency

--- aoc/src/bin/day3/main.rs
+++ aoc/src/bin/day3/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::fs;
+
+fn main() {
+    let file_path = "day3/day3.txt";
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let backpacks: Vec<&str> = contents.split("\n").collect();
+
+    let mut total_sum = 0;
+
+    for bag in backpacks.into_iter() {
+        let first_compartment = bag.to_string().substring(0, 3);
+    }
+}
