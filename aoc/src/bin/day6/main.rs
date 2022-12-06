5032642854d237aed42d53ed44668671d43ed709
phillip-kemper <phillip-kemper@gmx.net>
2022-12-06 12:43:55 +0100

    task 1

--- aoc/src/bin/day6/main.rs
+++ aoc/src/bin/day6/main.rs
@@ -1,9 +1,21 @@
 use std::fs;
 
 fn main() {
-    let file_path = "day5/day5.txt";
+    let file_path = "day6/day6.txt";
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let rows: Vec<&str> = contents.split("\n").collect();
+    for (index, _char) in contents.chars().enumerate() {
+        let mut four_connected_chars = vec![];
+        for i in index..index + 4 {
+            four_connected_chars.push(contents.chars().nth(i).unwrap());
+        }
+        println!("{:?}", four_connected_chars);
+        four_connected_chars.sort();
+        four_connected_chars.dedup();
+        if four_connected_chars.len() == 4 {
+            println!("Starts at position: {:?}", index + 4);
+            return;
+        }
+    }
 }
