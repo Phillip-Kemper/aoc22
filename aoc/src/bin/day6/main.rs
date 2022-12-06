20f63778d2bd4bd079225477b520080001271b76
phillip-kemper <phillip-kemper@gmx.net>
2022-12-06 12:46:14 +0100

    task 2

--- aoc/src/bin/day6/main.rs
+++ aoc/src/bin/day6/main.rs
@@ -5,16 +5,18 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
+    let marker_length = 14;
+
     for (index, _char) in contents.chars().enumerate() {
         let mut four_connected_chars = vec![];
-        for i in index..index + 4 {
+        for i in index..index + marker_length {
             four_connected_chars.push(contents.chars().nth(i).unwrap());
         }
         println!("{:?}", four_connected_chars);
         four_connected_chars.sort();
         four_connected_chars.dedup();
-        if four_connected_chars.len() == 4 {
-            println!("Starts at position: {:?}", index + 4);
+        if four_connected_chars.len() == marker_length {
+            println!("Starts at position: {:?}", index + marker_length);
             return;
         }
     }
